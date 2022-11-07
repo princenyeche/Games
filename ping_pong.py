2633ca05239300818523d807054ed1cab318a7d1
Prince <hakwumerunyeche@gmail.com>
2022-11-07 17:19:53 +0100

    added automated users and human users
    * You can easily switch between automated users or human users

--- ping_pong.py
+++ ping_pong.py
@@ -10,7 +10,6 @@ Author: Prince Nyeche
 | .__/|_|_| |_|\__, | .__/ \___/|_| |_|\__, |
 | |             __/ | |                 __/ |
 |_|            |___/|_|                |___/
-
 A Ping Pong Game made on Python 3.7
 """
 import turtle
@@ -20,7 +19,9 @@ import json
 
 WIDTH = 800
 HEIGHT = 600
-FILE = "config.json"  # should be in the same path where this pingpong.py file is run
+# The below should be in the same path where this pingpong.py file is run
+FILE = "config.json"
+
 CONFIG = None
 if os.path.isfile(FILE):
     CONFIG = json.load(open('config.json'))
@@ -70,40 +71,71 @@ PEN.color("white")
 PEN.penup()
 PEN.hideturtle()
 PEN.goto(0, 260)
-PEN.write(f"{CONFIG['player_one']}: 0 {CONFIG['player_two']}: 0",
+PEN.write(f"{CONFIG['player_name_one']}: 0 {CONFIG['player_name_two']}: 0",
           align="center", font=("Courier", 24, "normal"))
 
 
 # automated user
-# Player A or CPU
-def player_cpu() -> None:
-    """An automated cpu user."""
+# Player A or B or CPU
+def cpu(attr: bool = True) -> None:
+    """An automated cpu user.
+
+    :param attr: Switches context between left and right
+     paddle
+
+    :return: None
+    """
     # get the ball position and compare coordinates
     get_ball = BALL.pos()
     ball_cord = BALL.xcor(), BALL.ycor()
 
     def movement():
         # define the speed for paddle of cpu user 0=fastest, 1=slowest
-        PADDLE_A.speed(CONFIG['menu']['speed'])
+        content = PADDLE_A if attr is True else PADDLE_B
+        content.speed(CONFIG['menu']['speed'])
+        y_axis = ball_cord > (10, 50) if attr is \
+                                         True else ball_cord > (30, -30)
+        x_axis = ball_cord < (-50, -10) if attr is \
+                                           True else ball_cord < (-30, -60)
+        player = CONFIG['player_name_one'] \
+            if attr is True else CONFIG['player_name_two']
         if get_ball:
-            if (-340 > BALL.xcor() > -350) and (PADDLE_A.ycor() + 50 > BALL.ycor()
-                                                > PADDLE_A.ycor() - 50):
+            # get ball/paddle collision
+            values = -340 > BALL.xcor() > -350 if attr is True else \
+                340 < BALL.xcor() < 350
+            if values and (content.ycor() + 50
+                           > BALL.ycor() > content.ycor() - 50):
                 BALL.dx *= -1
                 sound_on()
-            # Look at the y-axis left-side
-            if ball_cord > (10, 50):
-                if PADDLE_A.towards(get_ball) < 50:
-                    paddle_a_up()
-                    print(f"{CONFIG['player_one']}: moving up")
-            # Look at the x-axis left-side
-            elif ball_cord < (-50, -10):
-                if PADDLE_A.towards(get_ball) > 100:
-                    paddle_a_down()
-                    print(f"{CONFIG['player_one']}: moving down")
-                # correct movement along this axis
-                elif PADDLE_A.towards(get_ball) < 50:
-                    paddle_a_up()
-                    print(f"{CONFIG['player_one']}: moving up")
+
+            # Look at the y-axis right-side
+            if y_axis:
+                my_value = 50 if attr is True else 180
+                if content.towards(get_ball) < my_value:
+                    paddle_a_up() if attr is \
+                                     True else paddle_b_up()
+                    print(f"{player}: moving up")
+                else:
+                    if attr is False:
+                        if content.towards(get_ball) > 20:
+                            paddle_b_down()
+                            print(f"{player}: moving down")
+                            # Look at the x-axis right-side
+            elif x_axis:
+                symbol = content.towards(get_ball) < 150 if attr is False \
+                    else content.towards(get_ball) > 100
+                if symbol:
+                    (paddle_a_up() if attr is True else paddle_b_up()) \
+                        if attr is False else \
+                        (paddle_a_down() if attr is True else paddle_b_down())
+                    print("{}: {}".format(player, "moving up"
+                    if attr is False else "moving down"))
+                else:
+                    if attr is True:
+                        # correct movement along this axis
+                        if content.towards(get_ball) < 50:
+                            paddle_a_up()
+                            print(f"{player}: moving up")
 
     movement()
 
@@ -167,7 +199,7 @@ def main():
     score_a = 0
     score_b = 0
     global GAME_SPEED
-    keep_score_track = 0
+    keep_score_track_a, keep_score_track_b = score_a, score_b
     idle_time = 0
     while True:
         WINDOW.update()
@@ -177,13 +209,16 @@ def main():
         BALL.sety(BALL.ycor() + BALL.dy)
 
         def speed_up():
+            # Paddle and ball collision
             global GAME_SPEED
             GAME_SPEED -= 0.001
             BALL.speed(GAME_SPEED)
             if idle_time == 15:
-                BALL.dx *= -2.5
+                # This overrides the ball_speed_limit for the first time only
+                BALL.dx *= CONFIG["ball_speed"]
             else:
-                BALL.dx *= -1.1 if BALL.dx < abs(CONFIG['ball_speed_limit']) else -1
+                BALL.dx *= -1.1 if BALL.dx < \
+                                   abs(CONFIG['ball_speed_limit']) else -1
 
         # boarder checking for ball then make sound
         # top
@@ -202,7 +237,8 @@ def main():
         if BALL.xcor() > 390:
             score_a += 1
             PEN.clear()
-            PEN.write(f"{CONFIG['player_one']}: {score_a} {CONFIG['player_two']}: {score_b}",
+            PEN.write(f"{CONFIG['player_name_one']}: {score_a} "
+                      f"{CONFIG['player_name_two']}: {score_b}",
                       align="center", font=("Courier", 24, "normal"))
             BALL.goto(0, 0)
             speed_up()
@@ -211,40 +247,57 @@ def main():
         elif BALL.xcor() < -390:
             score_b += 1
             PEN.clear()
-            PEN.write(f"{CONFIG['player_one']}: {score_a} {CONFIG['player_two']}: {score_b}",
+            PEN.write(f"{CONFIG['player_name_one']}: {score_a} "
+                      f"{CONFIG['player_name_two']}: {score_b}",
                       align="center", font=("Courier", 24, "normal"))
             BALL.goto(0, 0)
             speed_up()
 
-        # Paddle and ball collision
-        # right paddle
-        if (340 < BALL.xcor() < 350) and (PADDLE_B.ycor() + 50 > BALL.ycor() > PADDLE_B.ycor() - 50):
-            BALL.dx *= -1
-            sound_on()
-
         if CONFIG["menu"]:
             # change the menu->player_one=robot for cpu user
             if CONFIG["menu"]["player_one"] == "robot".lower():
-                keep_score_track = score_a
-                player_cpu()
-            else:
-                # first player left paddle if menu->player_one=human in config file
+                keep_score_track_a = score_a
+                cpu(True)
+            if CONFIG["menu"]["player_two"] == "robot".lower():
+                keep_score_track_b = score_b
+                cpu(False)
+
+            if CONFIG["menu"]["player_one"] != "robot".lower():
+                # first player left paddle if menu->player_one=human
+                # in config file
                 # "WS" keyboard use "w" for up, "s" for down
-                if (-340 > BALL.xcor() > -350) and (PADDLE_A.ycor() + 50 >
-                                                    BALL.ycor() > PADDLE_A.ycor() - 50):
+                if (-340 > BALL.xcor() > -350) and \
+                        (PADDLE_A.ycor() + 50 >
+                         BALL.ycor() > PADDLE_A.ycor() - 50):
+                    BALL.dx *= -1
+                    sound_on()
+            if CONFIG["menu"]["player_two"] != "robot".lower():
+                # second player right paddle if menu->player_two=human
+                # in config file
+                # "up/down arrow keys" keyboard use "up" for up,
+                # "down" for down
+                if (340 < BALL.xcor() < 350) and \
+                        (PADDLE_B.ycor() + 50 > BALL.ycor()
+                         > PADDLE_B.ycor() - 50):
                     BALL.dx *= -1
                     sound_on()
 
-        if keep_score_track >= CONFIG['score_limit']:
+        auto_score = keep_score_track_a \
+            if keep_score_track_a else keep_score_track_b \
+            if keep_score_track_b else 0
+        if auto_score >= CONFIG['score_limit']:
             WINDOW.bgcolor("black")
             PEN.clear()
+            GAME_SPEED = 0.1
             PADDLE_A.goto(800, 800)
             PADDLE_B.goto(800, 800)
             BALL.goto(0, 0)
             BALL.hideturtle()
-            PEN.write(f"{CONFIG['player_one']}: {score_a} {CONFIG['player_two']}: {score_b}",
+            PEN.write(f"{CONFIG['player_name_one']}: {score_a} "
+                      f"{CONFIG['player_name_two']}: {score_b}",
                       align="center", font=("Courier", 24, "normal"))
-            BALL.write("GAME OVER", align="center", font=("Courier", 40, "normal"))
+            BALL.write("GAME OVER", align="center",
+                       font=("Courier", 40, "normal"))
 
         if idle_time == 15:
             speed_up()
